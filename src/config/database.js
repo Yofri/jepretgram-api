34c03545c3085c745f5745c6df2e7e5b773e0a28
Yofriadi Yahya <yofriadiyahya@gmail.com>
2018-01-17 14:28:38 +0700

    Ready to deploy

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
-mongoose.connect(process.env.DB_URI)
+mongoose.connect(process.env.MLAB_URI)
 mongoose.Promise = global.Promise
 const connection = mongoose.connection
 
